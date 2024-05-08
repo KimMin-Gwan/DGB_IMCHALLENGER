0fcc530c830827bf5c44e660beb3194d260fbd59
hyellm <yhl9779@naver.com>
2024-05-08 15:44:32 +0900

    ha.....

--- dgb_game/lib/src/ui/setting_widget.dart
+++ dgb_game/lib/src/ui/setting_widget.dart
@@ -22,7 +22,7 @@ class SettingWidget extends StatefulWidget {
 // 모르겠으면 home_widget.dart 참고할것
 class _SettingWidgetState extends State<SettingWidget> {
 
-  var _style = SettingTheme();  // 테마
+  var _style = SettingTheme(); // 테마
   final maxWidth = 400.0;
   final maxHeight = 900.0;
 
@@ -31,32 +31,35 @@ class _SettingWidgetState extends State<SettingWidget> {
   @override
   Widget build(BuildContext context) {
     // 화면의 가로 세로 길이를 받아오기
-    double queryWidth = MediaQuery.of(context).size.width;
+    double queryWidth = MediaQuery
+        .of(context)
+        .size
+        .width;
     // 가로 최대 길이를 400으로 한정
-    if (queryWidth > maxWidth) {queryWidth = maxWidth;}
-    double queryHeight = MediaQuery.of(context).size.height;
+    if (queryWidth > maxWidth) {
+      queryWidth = maxWidth;
+    }
+    double queryHeight = MediaQuery
+        .of(context)
+        .size
+        .height;
     // 세로 최대 길이를 1200으로  한정
-    if (queryHeight > maxHeight) {queryHeight = maxHeight; }
+    if (queryHeight > maxHeight) {
+      queryHeight = maxHeight;
+    }
 
     // 가장 큰 틀!!!!!!!!!
     return Scaffold(
         body: Container(
-            color: Color(0xFFEFF6FE),  // 배경 색깔 지정 getMainWhite()
+            color: Color(0xFFEFF6FE), // 배경 색깔 지정 getMainWhite()
             width: queryWidth, // 화면 가로 길이 받아온걸로 지정
             height: queryHeight, // 화면 높이 받아온걸로 지정
             child: Column(
               children: [
-<<<<<<< Updated upstream
-                Header(queryWidth, queryHeight),  // 최상단
-                Title(queryWidth, queryHeight)  // 설정 제목
+                Header(queryWidth, queryHeight), // 최상단
+                Title(queryWidth, queryHeight) // 설정 제목
               ],
             )
-=======
-                Header(queryWidth, queryHeight),
-              ]
-            )
-            //child: Header(queryWidth, queryHeight)  // 이런식으로 child 추가해 나갈것
->>>>>>> Stashed changes
         )
     );
   }
@@ -82,34 +85,31 @@ class _SettingWidgetState extends State<SettingWidget> {
     );
   }
 
-  Widget
 // ----------------------------------------------------------------------------------
 // Title 함수
-Widget Title_text(width, height) {
-  return Container(
-    alignment: Alignment.centerLeft,
-    child: Text(
-      "  설정", // 여기에 원하는 텍스트를 입력하세요.
-      style: TextStyle(
-        fontSize: 24, // 텍스트 크기를 지정합니다.
-        fontWeight: FontWeight.bold, // 글자 굵기를 지정합니다.
-        color: Colors.black, // 텍스트 색상을 지정합니다.
+  Widget Title_text(width, height) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      child: Text(
+        "  설정", // 여기에 원하는 텍스트를 입력하세요.
+        style: TextStyle(
+          fontSize: 24, // 텍스트 크기를 지정합니다.
+          fontWeight: FontWeight.bold, // 글자 굵기를 지정합니다.
+          color: Colors.black, // 텍스트 색상을 지정합니다.
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
 
-Widget Title_image(width, height) {
-  return Padding( // 여기에 Padding 위젯을 추가합니다.
-    padding: EdgeInsets.only(left: 15.0), // 왼쪽에 10.0의 패딩을 추가합니다.
-    child: Image.asset(
-      'assets/setting.png', // 이미지 경로
-      width: 50, // 이미지의 가로 크기
-      height: 50, // 이미지의 세로 크기
-    ),
-  );
-}
-
-
+  Widget Title_image(width, height) {
+    return Padding( // 여기에 Padding 위젯을 추가합니다.
+      padding: EdgeInsets.only(left: 15.0), // 왼쪽에 10.0의 패딩을 추가합니다.
+      child: Image.asset(
+        'assets/setting.png', // 이미지 경로
+        width: 50, // 이미지의 가로 크기
+        height: 50, // 이미지의 세로 크기
+      ),
+    );
+  }
 
 }
